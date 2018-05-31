072e978318046ef2a59529c15f0b1ebbee88ae15
Marcus <marcus.hibell@roche.com>
2018-05-31 05:48:50 +0200

    test of my timeline coming form Mongodb - not working

---
+++ timeline.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, RadioField, validators
+import requests
+import json
+import os
+import codecs
+import pandas as pd
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from pymongo import MongoClient # Database connector
+from bson.objectid import ObjectId # For ObjectId to work
+
+#APP settings
+app = Flask(__name__)
+
+
+#CONNECT TO MONGODB
+client = MongoClient('localhost', 27017)    #Configure the connection to the database
+db = client.timeline                        #Select the database
+tline = db.study                            #Select the collection
+
+title = "Timeline Example"
+heading = "List Details"
+#modify=ObjectId()
+
+@app.route("/home")
+def home():
+	return render_template('index.html',t=title,h=heading)
+
+@app.route("/tline")
+def lists ():
+    #Display the all Study data in the timeline
+    tline_l = tline.find()
+    print(tline_l)
+    a1="active"
+    return render_template('timeline.html',a1=a1,tline_l=tline_l,t=title,h=heading)
+
+@app.route("/about")
+def about():
+	return render_template('credits.html',t=title,h=heading)
+
+
+if __name__ == "__main__":
+    app.secret_key='secret123'
+    app.run('0.0.0.0',5005,debug=True)
